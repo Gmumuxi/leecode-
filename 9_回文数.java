4abae4ffefae7b12580398406a1061319bdb45cd
Grace <1106518271@qq.com>
2022-07-09 11:26:26 +0800

    Create 9_回文数.java

---
+++ 9_回文数.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String int2Str = Integer.toString(x);
+        int intLen = int2Str.length();
+
+        for(int i=0; i < Math.floor(intLen)/2; i++){
+            if(int2Str.charAt(i) != int2Str.charAt(intLen-1-i)) {
+                return false;
+            } else {
+                return true;
+            }            
+        }
+        return true; 
+    }
+}
